d3a8a7f80086d7686d94ddded3bdcb36a5690b60
Humberto-Bonadiman <humberto_bonadiman@hotmail.com>
2022-10-20 20:38:27 -0400

    docs: fix documentation

--- src/main/java/com/java/spring/controller/UserController.java
+++ src/main/java/com/java/spring/controller/UserController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "Account endpoint")
+@Tag(name = "User endpoint")
 @CrossOrigin
 @RestController
 @RequestMapping("/user")
